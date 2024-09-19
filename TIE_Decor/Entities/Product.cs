21cde30570d5d161d3f46b224dfe70712c8fa005
luonghoantrongct2004 <luonghoantrongct2004@gmail.com>
2024-09-19 10:08:44 +0700

    Update Table Product

--- TIE_Decor/Entities/Product.cs
+++ TIE_Decor/Entities/Product.cs
@@ -6,7 +6,8 @@ public class Product
     public string ProductName { get; set; }
     public decimal Price { get; set; }
     public string Description { get; set; }
-    public string ImageUrl { get; set; }
+
+    public List<string>? ImageUrl { get; set; }
 
     // Quan hệ 1-nhiều với Review (Đánh giá)
     public Category? Category { get; set; }
