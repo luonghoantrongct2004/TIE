2b44366c0f064df9be761511eb613e461aa34812
Đăng Vip Pro <ntdanga22032@cusc.ctu.edu.vn>
2024-09-20 09:10:18 +0700

    Update Categories Find + Delete

--- TIE_Decor/Areas/Admin/Controllers/CategoriesController.cs
+++ TIE_Decor/Areas/Admin/Controllers/CategoriesController.cs
@@ -119,28 +119,9 @@ public class CategoriesController : Controller
     }
 
     // GET: Categories/Delete/5
-    public async Task<IActionResult> Delete(int? id)
+    public async Task<IActionResult> Delete(int id)
     {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var category = await _context.Categories
-            .FirstOrDefaultAsync(m => m.CategoryId == id);
-        if (category == null)
-        {
-            return NotFound();
-        }
-
-        return View(category);
-    }
-
-    // POST: Categories/Delete/5
-    [HttpPost, ActionName("Delete")]
-    public async Task<IActionResult> DeleteConfirmed(int CategoryId)
-    {
-        var category = await _context.Categories.FindAsync(CategoryId);
+        var category = await _context.Categories.FindAsync(id);
         if (category != null)
         {
             _context.Categories.Remove(category);
