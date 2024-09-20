17816eb4dbce18f0ee0cddc7102ac1f0dba8068a
luonghoantrongct2004 <luonghoantrongct2004@gmail.com>
2024-09-20 11:46:47 +0700

    update

--- TIE_Decor/Entities/Consultation.cs
+++ TIE_Decor/Entities/Consultation.cs
@@ -4,7 +4,7 @@ public class Consultation
 {
     public int ConsultationId { get; set; }  // ID của buổi tư vấn
     public int UserId { get; set; }  // ID của người dùng (chủ nhà)
-    public int DesignerId { get; set; }  // ID của nhà thiết kế
+    public string DesignerId { get; set; }  // ID của nhà thiết kế
     public DateTime ScheduledTime { get; set; }  // Thời gian diễn ra buổi tư vấn
     public string Status { get; set; } = "Đã đặt lịch";  // Trạng thái của buổi tư vấn (ví dụ: "Đã đặt lịch", "Hoàn thành", "Hủy")
     public string? Notes { get; set; }  // Ghi chú bổ sung về buổi tư vấn (cả người dùng và nhà thiết kế có thể thêm)
