f4fd303007b75071d80c3cdec4d7cb60667197a0
Y Nguyen Thi Ngoc <ngocybeauty@gmail.com>
2024-09-21 12:55:53 +0700

    index

---
+++ TIE_Decor/Entities/PageTimeSpent.cs
@@ -0,0 +1,10 @@
+﻿namespace TIE_Decor.Entities
+{
+    public class PageTimeSpent
+    {
+        public int Id { get; set; }
+        public string PageUrl { get; set; }
+        public int TimeSpent { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
---
+++ TIE_Decor/Entities/PageVisit.cs
@@ -0,0 +1,9 @@
+﻿namespace TIE_Decor.Entities
+{
+    public class PageVisit
+    {
+        public int Id { get; set; }
+        public string PageUrl { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
