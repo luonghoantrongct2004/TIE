f0a02463595747c3e15a279a1d0826de4cdf81c1
Gia Thoai <tngthoaia22043@cusc.ctu.edu.vn>
2024-09-21 16:27:29 +0700

    changeinformation

---
+++ TIE_Decor/Areas/Admin/Models/AdminProfileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace TIE_Decor.Areas.Admin.Models
+{
+    public class AdminProfileViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Fullname not null")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email not null"), EmailAddress(ErrorMessage = "Email not valid")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be 8 characters")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password is not match")]
+        public string ConfirmPassword { get; set; }
+        public string Phone { get; set; }
+
+        [Display(Name = "Profile Image")]
+        public string? ImageUrl { get; set; }
+    }
+}
